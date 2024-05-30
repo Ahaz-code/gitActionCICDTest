a772637ce67ed39ed23d035e28bf6080f17bbdc2
yidnekachew <yidnekachew.esmail@gmail.com>
2024-05-30 16:18:41 +0300

    my test ci/cd

---
+++ src/test/java/com/example/gitActionCICDTest/GitActionCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitActionCICDTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionCicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
